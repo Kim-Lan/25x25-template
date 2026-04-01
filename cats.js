663cf8601c4741021e55d6a69ad7d19b5355a4be
Kim-Lan <me@kim-lan.com>
2026-03-31 20:13:52 -0700

    Initial commit

---
+++ cats.js
@@ -0,0 +1,55 @@
+const cats = {};
+
+// replace category names, keep quotes
+// paste lists between [] on the right side of the equal sign
+// example: cats["category name"] = ["item 1", "item 2", "item 3", ...];
+
+cats["category 1"] = [];
+
+cats["category 2"] = [];
+
+cats["category 3"] = [];
+
+cats["category 4"] = [];
+
+cats["category 5"] = [];
+
+cats["category 6"] = [];
+
+cats["category 7"] = [];
+
+cats["category 8"] = [];
+
+cats["category 9"] = [];
+
+cats["category 10"] = [];
+
+cats["category 11"] = [];
+
+cats["category 12"] = [];
+
+cats["category 13"] = [];
+
+cats["category 14"] = [];
+
+cats["category 15"] = [];
+
+cats["category 16"] = [];
+
+cats["category 17"] = [];
+
+cats["category 18"] = [];
+
+cats["category 19"] = [];
+
+cats["category 20"] = [];
+
+cats["category 21"] = [];
+
+cats["category 22"] = [];
+
+cats["category 23"] = [];
+
+cats["category 24"] = [];
+
+cats["category 25"] = [];
